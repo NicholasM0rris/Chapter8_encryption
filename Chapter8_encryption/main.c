4b5ed0f1363f9e6ab0da53255e918100fd601336
Nicholas Morris <PrinceMorris@nicholas-morriss-imac.local>
2018-12-14 13:34:10 +1000

    Basic encrpytion

--- Chapter8_encryption/main.c
+++ Chapter8_encryption/main.c
@@ -7,9 +7,59 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
 
+#define N 100
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    
+    int i = 0;
+    int length = 0;
+    int ch;
+    int shift;
+    int array[N] = {' '};
+    
+    //Prompt user to enter message to be encrypted
+    printf("Enter message to be encrypted: ");
+    while ((ch = getchar()) != '\n' && ch != EOF) {
+        array[i] = ch;
+        i++;
+        length++;
+      
+    }
+    //Prompt user for shift amount
+    printf("Enter shift amount (1-25): ");
+    scanf("%d", &shift);
+    //Shift the elements and give them values corresponding to 1-26 to A-Z
+    for(i = 0; i < N; i++){
+        if(array[i] >= 'A' && array[i] <= 'Z'){
+            array[i] = (unsigned int)((array[i] - 'A') + shift)%26;
+        }
+        else if (array[i] >= 'a' && array[i] <= 'z'){
+            array[i] = (unsigned int)(abs(((array[i] - 'a') + shift)%26))-26;
+        }
+        
+    }
+    //Conversions
+    for(i = 0; i < N; i++){
+        if(array[i] >= 0 && array[i] < 26){
+            array[i] = array[i] + 'A';
+        }
+        else if (array[i] >= -26 && array[i] < 0){
+            array[i] = array[i] + 'a'+26;
+        }
+        
+    }
+    
+    
+    //Print the array
+    for(i = 0; i < length; i++){
+        printf("%c", array[i]);
+    }
+    puts("\n");
+    
+    
 }
